8516b0529ffe6443548af5ceeb8328353e8b7808
Jay Barbhaiya <jay.barbhaiya@sap.com>
2021-10-15 15:45:12 +0200

    Ui5App sample with typescript

--- src/controller/App.controller.ts
+++ src/controller/App.controller.ts
@@ -1,6 +1,8 @@
-import MessageBox from "sap/m/MessageBox";
 import Controller from "sap/ui/core/mvc/Controller";
 import AppComponent from "../Component";
+import List from "sap/m/List";
+import StandardListItem from "sap/m/StandardListItem";
+import Event from "sap/ui/base/Event";
 
 /**
  * @namespace com.myorg.myapp.controller
@@ -10,9 +12,20 @@ export default class AppController extends Controller {
 	public onInit() : void {
 		// apply content density mode to root view
 		this.getView().addStyleClass((this.getOwnerComponent() as AppComponent).getContentDensityClass());
+
+		const oList: List = this.getView().byId("myList") as List;
+		const oFirstItem: StandardListItem = oList.getItems()[0] as StandardListItem;
+
+		oFirstItem.setSelected(true);
+		this.handleSelectionChange(undefined, oFirstItem);
 	}
 
-	public sayHello() : void {
-		MessageBox.show("Hello World!");
+	public handleSelectionChange(oEvent: Event, oListItem: StandardListItem) : void {
+		if (oEvent) {
+			alert((oEvent.getParameter("listItem") as StandardListItem).getId());
+		} else {
+			const sItemId: string = oListItem.getId();
+			alert(sItemId);
+		}
 	}
 }
